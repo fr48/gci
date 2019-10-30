c74a800844a21a5b015866d940e6efef699dbdcf
fr48 <fr48@columbia.edu>
2019-10-30 11:17:41 -0400

    Missing tooltips

--- src/scripts/chart-02.js
+++ src/scripts/chart-02.js
@@ -1,89 +1,179 @@
 import * as d3 from 'd3'
 
-// step 1: define your variables
-const var_height = 400
-const var_width = 700
-const margin = { top: 1, bottom: 1, left: 34, right: 34 }
+const margin = { top: 60, left: 50, right: 150, bottom: 30 }
 
-/* having margins give you some wiggle room in the chart so that it is spread out
-                  margin top
-        margin.left          margin.right
-                  margin.bottom
-this way you make sure that the graph isn't having stuff all on the edge. 
-*/
+const height = 600 - margin.top - margin.bottom
+const width = 450 - margin.left - margin.right
 
-/* 
-What is 'g'?
-it stands for 'group'
-the translate command is saying the exact coordinate of where to start the 
-top-left corner of the g (container). This way, we say that the 0,0 in the 
-svg is different from the 0,0 in g, and we want to use the g's coordinates 
-for all our drawing.
-*/
-
-// step 2: create your svg
 const svg = d3
   .select('#chart-2')
   .append('svg')
-  .attr('height', var_height + margin.top + margin.bottom)
-  .attr('weight', var_width + margin.left + margin.right)
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
   .append('g')
   .attr('transform', `translate(${margin.left},${margin.top})`)
 
-/* what does d3 stand for?
-  Data Driven Documents
+const xPositionScale = d3
+  .scalePoint()
+  .domain(['2014', '2016', '2018'])
+  .range([0, width])
 
-  to add the file, you need a require() and a ready function
-  rule of thumb: put components you need to assemble (Scales, variables) 
-  outside of ready, and the actual assembly instructions inside ready
-  */
-
-// step 4: let's add data
-d3.csv(require('../data/trends.csv')).then(ready)
-
-/* 
-we will leave out the domain on countries for the time being 
-clamp() is a useful function that basically says, if you have 
-data that doesn't fit the domain, set to minimum value 
-*/
-// Step 5a: create your scales
-const xPositionScale = d3.scaleBand().range([0, var_width])
 const yPositionScale = d3
   .scaleLinear()
-  .domain([45, 110])
-  .range([0, var_height])
-/*
-what you call the data being read in is up to you for the ready function. 
-datapoints is just convention
+  .domain([0.2, 1])
+  .range([height, 0])
 
-*/
+const colorScale = d3
+  .scaleOrdinal()
+  .range([
+    '#a6cee3',
+    '#1f78b4',
+    '#b2df8a',
+    '#33a02c',
+    '#fb9a99',
+    '#e31a1c',
+    '#fdbf6f',
+    '#ff7f00',
+    '#cab2d6',
+    '#6a3d9a',
+    '#ffff99',
+    '#b15928',
+    '#4cc1fc',
+    '#333333'
+  ])
+
+const line = d3
+  .line()
+  .x(function(d) {
+    return xPositionScale(d.year)
+  })
+  .y(function(d) {
+    return yPositionScale(d.score)
+  })
+  .curve(d3.curveMonotoneX)
+
+d3.csv(require('../data/trends.csv')).then(ready)
+
 function ready(datapoints) {
-  // console.log(datapoints)
-  // step 5b: add your domain
-  const countries = d3.map(datapoints, d => d.country)
-  xPositionScale.domain(countries)
-  // step 5: draw a bar chart
-  /*
-  you need a scale, and ways to draw the rectangles, axes,
-  */
-  // step 5c: start drawing
+  console.log(datapoints)
+
+  const countries = datapoints.map(d => d.country)
+  colorScale.domain(countries)
+
+  const nested = d3
+    .nest()
+    .key(function(d) {
+      return d.country
+    })
+    .entries(datapoints)
+  console.log(nested)
+
+  // still missing: tooltip code
   svg
-    .selectAll('rect')
+    .selectAll('circle')
     .data(datapoints)
     .enter()
-    .append('rect')
-    .attr('x', xPositionScale(d => d.country))
-    .attr('y', 0)
-    .attr('width', xPositionScale.bandwidth())
-    .attr('height', d => yPositionScale(+d.life_expectancy))
-}
+    .append('circle')
+    .attr('cx', d => xPositionScale(d.year))
+    .attr('cy', d => yPositionScale(d.score))
+    .attr('r', 3)
+    .attr('fill', d => colorScale(d.country))
+    .attr('class', d => d.country)
+    .on('mouseover', function(d) {
+      d3.select(this)
+        .transition()
+        .duration(200)
+        .attr('r', 7)
+    })
+    .on('mouseout', function(d) {
+      d3.select(this)
+        .transition()
+        .duration(200)
+        .attr('r', 3)
+    })
 
-// step 3: draw in the 'g' inside the 'svg'
-/* harry's code showed a circle, mine didn't */
-/* commenting out in step 4
-svg
-  .append('circle')
-  .attr('r', 20)
-  .attr('cx', var_width / 2)
-  .attr('cy', var_height / 2)
-*/
+  svg
+    .selectAll('path')
+    .data(nested)
+    .enter()
+    .append('path')
+    .attr('stroke', function(d) {
+      return colorScale(d.key)
+    })
+    .attr('stroke-width', 1.5)
+    .attr('fill', 'none')
+    .attr('d', function(d) {
+      return line(d.values)
+    })
+    .attr('class', d => {
+      return d.key.toLowerCase()
+    })
+
+  svg
+    .selectAll('text')
+    .data(nested)
+    .enter()
+    .append('text')
+    .attr('font-size', 12)
+    .attr('fill', '#333333')
+    .attr('x', xPositionScale('2018'))
+    .attr('dx', 5)
+    .attr('y', function(d) {
+      return yPositionScale(d.values[0].score)
+    })
+    .text(function(d) {
+      if (d.key == 'United States of America') {
+        return d.values[0].score + 'USA'
+      }
+      return d.values[0].score + ' ' + d.key
+    })
+    .attr('dy', function(d) {
+      if (d.key === 'China') {
+        return 6
+      }
+      if (d.key == 'Russian Federation') {
+        return -4
+      }
+      if (d.key == 'Singapore') {
+        return 5
+      }
+      if (d.key == 'United Kingdom') {
+        return -3
+      }
+      if (d.key == 'United States of America') {
+        return 9
+      }
+      return 0
+    })
+    .attr('class', d => {
+      return d.key.toLowerCase()
+    })
+
+  // set up the axes
+  const xAxis = d3.axisBottom(xPositionScale)
+
+  svg
+    .append('g')
+    .attr('class', 'axis x-axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis)
+    .lower()
+
+  const yAxis = d3
+    .axisLeft(yPositionScale)
+    .tickValues([0.4, 0.6, 0.8])
+    .tickSize(-width)
+
+  svg
+    .append('g')
+    .attr('class', 'axis y-axis')
+    .call(yAxis)
+    .lower()
+
+  svg.selectAll('.y-axis path').remove()
+  svg.selectAll('.y-axis text').remove()
+  svg
+    .selectAll('.y-axis line')
+    .attr('stroke-dasharray', 2)
+    .attr('stroke', 'grey')
+}
